a7a14f79a35801b7fa7d02996e21db6482c2812b
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-14 22:43:54 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -22,17 +22,20 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 chmod 777 ./ngrok
 ./ngrok authtoken $ngrok_authtoken
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update -y
-sudo apt install apt-transport-https sublime-text xfce4 xarchiver mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y
+
+printf "\n\n Instaling.....\n"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - > /dev/null 2>&1
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null 2>&1
+sudo apt update -y > /dev/null 2>&1
+sudo apt install apt-transport-https sublime-text xfce4 xarchiver mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y > /dev/null 2>&1
 export HOME="$(cd ~/; pwd)"
 export DISPLAY=":0"
-mkdir ~/.vnc 2> /dev/null
-chmod -R 777 ~/.config
-printf '#!/bin/bash\ndbus-launch &> /dev/null\nautocutsel -fork\nxfce4-session\n' > ~/.vnc/xstartup
-chmod 777 ~/.vnc/xstartup
+mkdir ~/.vnc > /dev/null 2>&1
+chmod -R 777 ~/.config > /dev/null 2>&1
+printf '#!/bin/bash\ndbus-launch &> /dev/null\nautocutsel -fork\nxfce4-session\n' > ~/.vnc/xstartup 2> /dev/null
+chmod 777 ~/.vnc/xstartup > /dev/null 2>&1
 
+printf "\n\n Starting.....\n"
 unset DBUS_LAUNCH
 nohup sudo ./ngrok tcp --region ${ngrok_region} 127.0.0.1:5900 &> /dev/null &
 vncserver -kill :0 &> /dev/null 2> /dev/null
